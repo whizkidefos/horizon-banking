e9cd8f7f2bd5e7092eb0b9ab837e833f88a574f8
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2024-05-03 16:54:50 +0100

    Routes and layout architecture

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const ibmPlexSerif = IBM_Plex_Serif({
   subsets: ["latin"],
-  weight: ["400,700"],
+  weight: ["400", "700"],
   variable: "--font-ibm-plex-serif",
 });
 
