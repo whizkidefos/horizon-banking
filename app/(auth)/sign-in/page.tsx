348e2f99b7791bdc445f02c8afecf20a280f0cce
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2025-01-16 07:33:15 +0000

    wip: building auth screens

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,10 @@
+import AuthForm from "@/components/AuthForm";
 
-
-const SignIn = () => {
+const SignIn = async () => {
   return (
-    <div>SignIn</div>
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-in" />
+    </section>
   )
 }
 
